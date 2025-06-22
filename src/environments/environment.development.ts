273cf44a694d8db3209f8a32489bf5d0ec2d03cd
IvanNunez88 <ivanezf@gmail.com>
2025-06-21 18:41:36 -0600

    Segunta subida

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // endpoint: 'http://localhost:5029/api/' //LOCAL
-    endpoint: 'http://155.254.244.107/APIRAD/api/' //SERVER
+    endpoint: 'http://155.254.244.107/APIRAD/api/' //SERVE
 };
